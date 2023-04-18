e022bc4086f8f95bafdd5dfac36b005108904476
Tanishq Kashyap <76959062+Tanishq2505@users.noreply.github.com>
2023-04-18 09:36:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,4 +43,4 @@ def predict():
     return Response(response=base64_image, content_type='text/plain')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
